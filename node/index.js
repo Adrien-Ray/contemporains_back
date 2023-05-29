c3956b16f5987cbe5d68472fffa3b199a835e861
Adrien-Ray <a.raymond@codeur.online>
2023-05-29 13:50:42 +0200

    services docker works

---
+++ node/index.js
@@ -0,0 +1,29 @@
+// TIME ZONE
+process.env.TZ = 'Europe/Amsterdam';
+const today = new Date();
+console.info('the node date: ', today.toString());
+// DEPENDENCIES
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+// const { json } = require('express');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// USE PORT
+const port = 8080;
+
+// RUN SERVER
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+// TEST RUN ROOT
+app.get('/', (req, res) => {
+    console.log(`tu tape sur node / sombre con!`);
+    res.send('hello node in perso_www world! test nodemon');
+});
+
