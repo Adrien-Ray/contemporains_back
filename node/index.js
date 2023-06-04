041e0cc51d66376661f794123078a076cb60504d
Adrien-Ray <a.raymond@codeur.online>
2023-06-04 18:47:05 +0200

    all users routes exist now

--- node/index.js
+++ node/index.js
@@ -24,7 +24,14 @@ app.listen(port, () => {
 // TEST RUN ROOT
 app.get('/', (req, res) => {
     console.log(`tu tape sur node / !`);
-    res.send('hello node !');
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = ('0' + (now.getMonth() + 1)).slice(-2);
+    const day = ('0' + now.getDate()).slice(-2);
+    const hours = ('0' + now.getHours()).slice(-2);
+    const minutes = ('0' + now.getMinutes()).slice(-2);
+    const formattedDate = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes;
+    res.send(`Hello node ! It's ${formattedDate} o'clock`);
 });
 
 const router = express.Router();
--- node/src/controlers/users.js
+++ node/src/controlers/users.js
@@ -1,6 +1,22 @@
 module.exports = (router) => {
+    // login user
+    router.post('/users/login', async (req, res) => {
+        const rb = req.body;
+        return res.status(200).json({ message: 'road of login' });
+    });
+    // create user
     router.post('/users', async (req, res) => {
         const rb = req.body;
         return res.status(200).json({ message: 'you\'re in create user root' });
+    });
+    // modify user
+    router.patch('/users/:id', async (req, res) => {
+        const rb = req.body;
+        return res.status(200).json({ message: 'road of modify user' });
+    });
+    // delete user
+    router.delete('/users/:id', async (req, res) => {
+        const rb = req.body;
+        return res.status(200).json({ message: 'road of delete user' });
     });
 }
